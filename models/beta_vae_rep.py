8a6c681a911cc4c91667ab2a81505a140ca39503
arielkes <ariel.keslassy@weizmann.ac.il>
2022-03-02 11:08:23 +0000

    vae_rep.py and beta_vae_rep.py included blond hair representation: after the creation of the latent matrix z, it's first vector z[;,0] is passed to a sigmoid and then into BCE with the blond hair labels. this should enforce blond hair representation in the latent space. need to add:t-sne visualization

--- models/beta_vae_rep.py
+++ models/beta_vae_rep.py
@@ -5,17 +5,28 @@ from torch.nn import functional as F
 from .types_ import *
 
 
-class VAE_REP(BaseVAE):
+class BetaVAE(BaseVAE):
 
+    num_iter = 0 # Global static variable to keep track of iterations
 
     def __init__(self,
                  in_channels: int,
                  latent_dim: int,
                  hidden_dims: List = None,
+                 beta: int = 4,
+                 gamma:float = 1000.,
+                 max_capacity: int = 25,
+                 Capacity_max_iter: int = 1e5,
+                 loss_type:str = 'B',
                  **kwargs) -> None:
-        super(VAE_REP, self).__init__()
+        super(BetaVAE, self).__init__()
 
         self.latent_dim = latent_dim
+        self.beta = beta
+        self.gamma = gamma
+        self.loss_type = loss_type
+        self.C_max = torch.Tensor([max_capacity])
+        self.C_stop_iter = Capacity_max_iter
 
         modules = []
         if hidden_dims is None:
@@ -92,12 +103,6 @@ class VAE_REP(BaseVAE):
         return [mu, log_var]
 
     def decode(self, z: Tensor) -> Tensor:
-        """
-        Maps the given latent codes
-        onto the image space.
-        :param z: (Tensor) [B x D]
-        :return: (Tensor) [B x C x H x W]
-        """
         result = self.decoder_input(z)
         result = result.view(-1, 512, 2, 2)
         result = self.decoder(result)
@@ -106,32 +111,26 @@ class VAE_REP(BaseVAE):
 
     def reparameterize(self, mu: Tensor, logvar: Tensor) -> Tensor:
         """
-        Reparameterization trick to sample from N(mu, var) from
-        N(0,1).
-        :param mu: (Tensor) Mean of the latent Gaussian [B x D]
-        :param logvar: (Tensor) Standard deviation of the latent Gaussian [B x D]
-        :return: (Tensor) [B x D]
+        Will a single z be enough ti compute the expectation
+        for the loss??
+        :param mu: (Tensor) Mean of the latent Gaussian
+        :param logvar: (Tensor) Standard deviation of the latent Gaussian
+        :return:
         """
         std = torch.exp(0.5 * logvar)
         eps = torch.randn_like(std)
         return eps * std + mu
 
-    def forward(self, input: Tensor, **kwargs) -> List[Tensor]:
+    def forward(self, input: Tensor, **kwargs) -> Tensor:
         mu, log_var = self.encode(input)
         z = self.reparameterize(mu, log_var)
         blond = z[:, 0]
-        return [self.decode(z), input, mu, log_var, blond]
+        return  [self.decode(z), input, mu, log_var, blond]
 
     def loss_function(self,
                       *args,
                       **kwargs) -> dict:
-        """
-        Computes the VAE loss function.
-        KL(N(\mu, \sigma), N(0, 1)) = \log \frac{1}{\sigma} + \frac{\sigma^2 + \mu^2}{2} - \frac{1}{2}
-        :param args:
-        :param kwargs:
-        :return:
-        """
+        self.num_iter += 1
         recons = args[0]
         input = args[1]
         mu = args[2]
@@ -140,16 +139,25 @@ class VAE_REP(BaseVAE):
 
         blond_hair_labels = kwargs['labels'][:, 9].float()
         criterion = torch.nn.BCEWithLogitsLoss()
-
-        kld_weight = kwargs['M_N'] # Account for the minibatch samples from the dataset
-        recons_loss = F.mse_loss(recons, input)
-
-        kld_loss = torch.mean(-0.5 * torch.sum(1 + log_var - mu ** 2 - log_var.exp(), dim=1), dim=0)
-
         blond_hair_loss = criterion(blond_pred, blond_hair_labels)
-        loss = blond_hair_loss + recons_loss + kld_weight * kld_loss
 
-        return {'loss': loss,'Blond_Hair_Loss': blond_hair_loss.detach(), 'Reconstruction_Loss': recons_loss.detach(), 'KLD': -kld_loss.detach()}
+
+        kld_weight = kwargs['M_N']  # Account for the minibatch samples from the dataset
+
+        recons_loss =F.mse_loss(recons, input)
+
+        kld_loss = torch.mean(-0.5 * torch.sum(1 + log_var - mu ** 2 - log_var.exp(), dim = 1), dim = 0)
+
+        if self.loss_type == 'H': # https://openreview.net/forum?id=Sy2fzU9gl
+            loss = blond_hair_loss + recons_loss + self.beta * kld_weight * kld_loss
+        elif self.loss_type == 'B': # https://arxiv.org/pdf/1804.03599.pdf
+            self.C_max = self.C_max.to(input.device)
+            C = torch.clamp(self.C_max/self.C_stop_iter * self.num_iter, 0, self.C_max.data[0])
+            loss = blond_hair_loss + recons_loss + self.gamma * kld_weight* (kld_loss - C).abs()
+        else:
+            raise ValueError('Undefined loss type.')
+
+        return {'loss': loss, 'Reconstruction_Loss':recons_loss, 'KLD':kld_loss}
 
     def sample(self,
                num_samples:int,
